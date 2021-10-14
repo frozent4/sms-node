1696e94a6feaf6ece93efada62c188ca26ca38c3
frozenth4 <frozenthone321@gmail.com>
2021-10-14 11:52:05 -0500

    start

---
+++ src/twilio/send-test.js
@@ -0,0 +1,20 @@
+const config = require("../config");
+const client = require("twilio")('AC5256816ca09462355338e0d3295546a5', '9f74e0623ba9963874a6698cc67f0ff7');
+
+
+
+async function sendMessage() {
+    try {
+        const message = await client.messages.create({
+            to: '+51916090364',
+            from: '+12513133444',
+            body: 'sms-test',
+        });
+        console.log(message.sid)
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+sendMessage();
+console.log('---llegue aca')
